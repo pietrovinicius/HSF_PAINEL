213525838762478b5ba0397037996a28500c1ca8
Pietro Lima <pietrovinicius@hotmail.com>
2024-11-26 11:37:09 -0300

    Instalacao do Streamlit

---
+++ main.py
@@ -0,0 +1,9 @@
+#26/11/2023
+#@PLima
+#HFS - PAINEL DE DIVERSOS DADOS E INDICADORES
+
+
+
+    
+if __name__ == "__main__":    
+    print("============================== inicio ========================")
